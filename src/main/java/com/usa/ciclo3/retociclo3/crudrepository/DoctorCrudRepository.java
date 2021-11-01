f84ff980829fbac2890f041b124b0172fad41dae
Daniel Castro <49466923+danielcastro-97@users.noreply.github.com>
2021-11-01 06:01:30 -0500

    initial commit from project

---
+++ src/main/java/com/usa/ciclo3/retociclo3/crudrepository/DoctorCrudRepository.java
@@ -0,0 +1,7 @@
+package com.usa.ciclo3.retociclo3.crudrepository;
+
+import com.usa.ciclo3.retociclo3.model.Doctor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DoctorCrudRepository extends CrudRepository<Doctor, Integer> {
+}
