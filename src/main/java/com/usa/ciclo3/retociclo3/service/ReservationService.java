133401bb1286d2d5f78062e2ef79069f97a3b7f2
Daniel Castro <49466923+danielcastro-97@users.noreply.github.com>
2021-11-01 10:40:04 -0500

    The functionality for the reports was created

--- src/main/java/com/usa/ciclo3/retociclo3/service/ReservationService.java
+++ src/main/java/com/usa/ciclo3/retociclo3/service/ReservationService.java
@@ -1,10 +1,16 @@
 package com.usa.ciclo3.retociclo3.service;
 
 import com.usa.ciclo3.retociclo3.model.Reservation;
+import com.usa.ciclo3.retociclo3.reports.CountClient;
+import com.usa.ciclo3.retociclo3.reports.ReservationStatus;
 import com.usa.ciclo3.retociclo3.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -64,5 +70,29 @@ public class ReservationService {
             return true;
         }).orElse(false);
         return aBoolean;
+    }
+
+    public ReservationStatus getReservationStatusReport(){
+        List<Reservation> completed = reservationRepository.getReservationByStatus("Completed");
+        List<Reservation> cancelled = reservationRepository.getReservationByStatus("Cancelled");
+        return new ReservationStatus(completed.size(), cancelled.size());
+    }
+
+    public List<Reservation> getReservationPeriod(String dateOne, String dateTwo){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date startDate = dateFormat.parse(dateOne);
+            Date endDate = dateFormat.parse(dateTwo);
+            if (startDate.before(endDate)){
+                return reservationRepository.getReservationPeriod(startDate, endDate);
+            }
+        } catch (Exception exception){
+                exception.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    public List<CountClient> getTopClients(){
+        return reservationRepository.getTopClients();
     }
 }
